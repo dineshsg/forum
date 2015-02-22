b58734ef25442f542fc098873bfc1cd025b26df3
dineshsg <dineshsgaddi@gmail.com>
2015-02-23 02:17:16 +0530

    Solved bugs in the forum module

--- question.js
+++ question.js
@@ -1,6 +1,10 @@
 jQuery(document).ready(function($) {
-    webroot = "http://"+location.hostname+"/iParent1/";
+    webroot = "http://"+location.hostname+":85/iParent/";
 	
+	if(navigator.userAgent.match(/iPad/i))
+	{
+		$("#ask-a-question-form .form-type-file input").css("padding-left", "155px"); 
+	}
 	$('.grade').change(function(){
 	    grd = $('.grade').attr('value');
 	    $.ajax({
@@ -29,8 +33,19 @@ jQuery(document).ready(function($) {
     });
     
     $('.common').click(function() {
-        var grd = $(this).attr('id');
-		//var cnt_val=$('#hdn'+grd).val();
+        //var grd = $(this).attr('id');
+		if ($(this).attr('id').indexOf('title') > -1) 
+		{	
+			var grad = $(this).attr('id');
+			var grd=grad.substring(5); 
+			//alert();
+    	}
+		else
+		{
+			 var grd = $(this).attr('id');
+
+		}	
+		var cnt=$(this).attr('target');
         $.ajax({
 	        type : 'POST',
 	        url : webroot + "get_subject",
@@ -43,16 +58,58 @@ jQuery(document).ready(function($) {
 			    var html_data = "";
 			    if(output1){
 			        $.each($.parseJSON(data1), function(idx, obj) {
-			            html_data += "<span id='div"+obj.id+"' target='targetDiv' style='float:left;'><p id='"+ obj.id +"' style='border:1px solid #000;margin:25px;width:225px;background-color: #efefef;height:104px !important;'><a href='forum/questions/"+grd+"/"+obj.id+"/'>" + obj.subject_name + "</a></p></span>";	
-                    });
-                    
+						if(obj.subject_name=='Drama')							
+									imgname='Drama.png';
+							if(obj.subject_name=='Study Skills')							
+									imgname='StudySkills.png';							
+							if(obj.subject_name=='Mathematics')							
+									imgname='Mathematics.png';
+							if(obj.subject_name=='Language Skills')							
+									imgname='LanguageSkills.png';
+							if(obj.subject_name=='Reading Development')							
+									imgname='ReadingDevelopment.png';							
+							if(obj.subject_name=='Writing Development')							
+									imgname='WritingDevelopment.png';
+							if(obj.subject_name=='Learning Styles')							
+									imgname='LearningStyles.png';
+							if(obj.subject_name=='Project preparation')							
+									imgname='Projectpreparation.png';
+							if(obj.subject_name=='Research Skills')							
+									imgname='ResearchSkills.png';							
+							if(obj.subject_name=='Sports & Recreation')							
+									imgname='SportsRecreation.png';
+							if(obj.subject_name=='Sciences')							
+									imgname='Sciences.png';
+							if(obj.subject_name=='Life Skills')							
+									imgname='LifeSkills.png';
+							if(obj.subject_name=='Music')							
+									imgname='Music.png';
+							if(obj.subject_name=='Visual Arts')							
+									imgname='VisualArts.png';
+								
+			           /* html_data += "<span id='div"+obj.id+"' target='targetDiv' style='float:left;'><p id='"+ obj.id +"' style='border:1px solid #000;margin:25px;width:225px;background-color: #efefef;height:104px !important;'><a href='forum/questions/"+grd+"/"+obj.id+"/'>" + obj.subject_name + "</a></p></span>";	
+                    }); */
+                   var hlink="location.href='forum/questions/"+grd+"/"+obj.id+"/'";
+					
+					html_data += "<div id='div"+obj.id+"' target='targetDiv'  class='col-lg-3 col-md-3 col-sm-6 col-xs-12'  ><p id='"+ obj.id +"' style='border:1px solid #000;height:110px !important;background-image:url(./images/topics/"+imgname+");background-repeat:no-repeat;	background-position:center;text-align:center;cursor:pointer;' class='content-font topics-forum' onclick="+hlink+" ><a href='forum/questions/"+grd+"/"+obj.id+"/' class='font_black ' >" + obj.subject_name + "</a></p></div>";	
+					
+					});//background-image:url(./images/grades/Drama.png);<img src='./images/topics/Drama.png' /> background-color: #efefef;
                    $('.container_subjects').html(html_data);
-				   $('#container_subjects').focus();					
-                    window.scrollBy(0, 10000);
-				 
+
+
+
+				   $('#container_subjects').focus();
+
+					if($(window).width()<500){                   		
+							jQuery("html, body").animate({ scrollTop:715 },"slow");
+					}
+					if(($(window).width()>501) && ($(window).width()<700))
+					{
+							jQuery("html, body").animate({ scrollTop:435 },"slow");
+					}////window.scrollBy(0, 600);
 			    }
 			    else{
-			        alert('Somthing wrong, Please refresh page');
+			        alert('Something wrong, Please refresh page');
 			    }
 			}
 			
